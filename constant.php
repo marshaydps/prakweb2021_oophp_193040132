d9ede114d3cce72cae57d9680633c87412065ea0
Marsha Yunita D.P.S <60495671+marshaydps@users.noreply.github.com>
2021-09-24 16:29:11 +0700

    Mengerjakan Latihan Constant video ke 7

---
+++ constant.php
@@ -0,0 +1,24 @@
+<?php
+
+// define ('NAMA', 'Marsha Yunita D.P.S');
+// echo NAMA;
+
+// echo"<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// echo __LINE__; //menampilkan baris
+// echo __FILE__; //menampilkan alamat file
+
+// function coba() {
+//     return __FUNCTION__;
+//     echo coba();
+// }
+
+class coba {
+    public $kelas = __CLASS__;
+}
+    $obj = new coba;
+    echo $obj->kelas;
+?>
